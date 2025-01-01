9e23140c5623f70671dc2fe71dbac1f0b0f00df6
Mooogy <francismigsanjuan@gmail.com>
2024-12-31 20:54:28 -0800

    add LeafNode with unit tests

--- src/htmlnode.py
+++ src/htmlnode.py
@@ -14,11 +14,20 @@ class HTMLNode():
 
         attributes = ""
         for pair in self.props.items():
-            attributes = attributes + f" {pair[0]}=\"{pair[1]}\""
+            attributes = attributes + f"{pair[0]}=\"{pair[1]}\" "
 
-        return attributes
-    
+        return attributes[:len(attributes) - 1]
+
     def __repr__(self):
         return f"HTMLNode(<{self.tag}>, {self.value}, {self.children}, {self.props_to_html()})"
 
+class LeafNode(HTMLNode):
+    def __init__(self, tag, value, props=None):
+        super().__init__(tag, value, None, props)
+
+    def to_html(self):
+        if self.value == None: raise ValueError("All leaf nodes must have a value")
+        if self.tag == None: return self.value
+
+        return f"<{self.tag}{f" {self.props_to_html()}" if self.props else ""}>{self.value}</{self.tag}>"
     
--- src/test_htmlnode.py
+++ src/test_htmlnode.py
@@ -1,6 +1,6 @@
 import unittest
 
-from htmlnode import HTMLNode
+from htmlnode import HTMLNode, LeafNode
 
 class TestHTMLNode(unittest.TestCase):
     def test_repr(self):
@@ -16,7 +16,7 @@ class TestHTMLNode(unittest.TestCase):
             }
 
         node = HTMLNode("a", "Google", props=props)
-        expected_repr = "HTMLNode(<a>, Google, None,  href=\"https://google.com\" target=\"blank\")"
+        expected_repr = "HTMLNode(<a>, Google, None, href=\"https://google.com\" target=\"blank\")"
 
         self.assertEqual(repr(node), expected_repr)
     
@@ -28,7 +28,7 @@ class TestHTMLNode(unittest.TestCase):
             }
 
         node = HTMLNode(props=props)
-        expected_output = " href=\"https://google.com\" target=\"blank\""
+        expected_output = "href=\"https://google.com\" target=\"blank\""
 
         self.assertEqual(node.props_to_html(), expected_output)
     
@@ -43,3 +43,22 @@ class TestHTMLNode(unittest.TestCase):
         expected_output = " href=\"https://bing.com\" target=\"blank\""
 
         self.assertNotEqual(node.props_to_html(), expected_output)
+
+class TestLeafNode(unittest.TestCase):
+    def test_to_html(self):
+        node = LeafNode("p", "This is a paragraph of text.")
+        output = "<p>This is a paragraph of text.</p>"
+
+        self.assertEqual(node.to_html(), output)
+
+    def test_to_html_props(self):
+        node = LeafNode("a", "Click me!", {"href": "https://www.google.com"})
+        output = "<a href=\"https://www.google.com\">Click me!</a>"
+        
+        self.assertEqual(node.to_html(), output)
+
+    def test_to_html_rawtext(self):
+        node = LeafNode(None, "This is just raw text!")
+        output = "This is just raw text!"
+
+        self.assertEqual(node.to_html(), output)
